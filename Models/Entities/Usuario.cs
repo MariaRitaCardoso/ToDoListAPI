24b326b6a31df6594d84b4e98fc9b3e1f7108405
MariaRitaCardoso <maria.r.cardoso6@aluno.senai.br>
2026-03-26 13:51:33 -0300

    feat: Estrutura para as tabelas e migrations

---
+++ Models/Entities/Usuario.cs
@@ -0,0 +1,13 @@
+﻿using ToDoList.Models.Entities;
+
+namespace ToDoList.Models.DTOs
+{
+    public class Usuario
+    {
+        public Guid Id { get; set; }
+        public string Nome { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string PasswordHash { get; set; } = string.Empty;
+        public ICollection<Tarefa> Tarefas { get; set; } = new List<Tarefa>();
+    }
+}
